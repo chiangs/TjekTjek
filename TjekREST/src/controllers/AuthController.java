10e30d2f5a45fffc8f8d0d0ce26fa0fa566045b9
Developer <stephen.e.chiang@gmail.com>
2017-05-23 17:40:00 +0200

    REST controller route mapping ping/pong verified

--- TjekREST/src/controllers/AuthController.java
+++ TjekREST/src/controllers/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 public class AuthController {
 
 	@RequestMapping(path = "ping", method = RequestMethod.GET)
